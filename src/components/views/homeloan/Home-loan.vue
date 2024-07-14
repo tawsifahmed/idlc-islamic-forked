92eae6d68dcc232e4b14b91f6b8385fc2448f211
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2024-07-14 17:30:25 +0600

    previous profits tabs in deposit products added initially

--- src/components/views/homeloan/Home-loan.vue
+++ src/components/views/homeloan/Home-loan.vue
@@ -37,7 +37,96 @@
             <div class="header sectionTitle text-uppercase text-center">
               <h2 class="h2Responsive" style="margin-bottom: 30px;">Actual Previous Profits</h2>
             </div>
-            <div v-html="depositPSR" style="overflow-x: auto;"></div>
+            <pre>{{depoPrevProfits}}</pre>
+            <div class="row filterOptions">
+              <div class="col-md-12"  v-for="(tabData, index) in depoPrevProfits"   :key="tabData.id">
+                <b-card  class="product-card" style="background-color: #00a6b4;">
+                  <a  v-b-toggle="'annualReportcollapse' + index" block class="d-flex">
+                    <strong class="question_product" style="color:white; font-size: 1.25rem;">2024</strong>
+                    <!-- replace these with your preferred icons -->
+                    <strong v-if="isVisible" aria-hidden="true" class="ml-auto"><span class="icon"><span class="icon-caret-down" style="color: #fff; font-size: 18px;"></span></span></strong>
+                    <strong v-else aria-hidden="true" class="ml-auto"><span class="icon-caret-down" style="color: #fff; font-size: 18px;"></span></strong>
+                  </a>
+                  <b-collapse v-bind:id="'annualReportcollapse' + index">
+                    <b-card class="question-card">
+                      <a  v-b-toggle.collapse-home-loan-1 block>
+                        <div class="row">
+                          <div class="col-md-2" >
+                            <img :src="tabData.image" width="100%" alt="" v-if="tabData">
+                          </div>
+                          <div class="col-md-2 text-btn"  >
+                            <h6 class="annual_report_heading" v-if="tabData.title" >{{tabData.title}}</h6>
+                            <a :href="tabData.file" class="footer_button2" download target="_blank" v-if="tabData.title">Download</a>
+                          </div>
+
+                          <div class="col-md-8">
+                            <ul class="all-report float-right">
+                              <li class="" style="" v-if="tabData.file">
+                                <a :href="tabData.file" download target="_blank">
+                                  <div class="f_icon">
+                                    <img src="./../../../assets/img/financial-report/Q1.png" alt="">
+                                  </div>
+                                  <div class="title">
+                                    <h6>{{tabData.title}}</h6>
+                                  </div>
+                                  <div class="download-icon" style="padding: 5px 32px;">
+                                    <div class="d_icon"></div>
+                                  </div>
+                                </a>
+                              </li>
+                              <li class="" style=""  v-if="tabData.title">
+                                <a :href="tabData.file" download target="_blank">
+                                  <div class="f_icon">
+                                    <img src="./../../../assets/img/financial-report/Q2.png" alt="">
+                                  </div>
+                                  <div class="title">
+                                    <h6>{{tabData.title}}</h6>
+                                  </div>
+                                  <div class="download-icon" style="padding: 5px 32px;">
+                                    <div class="d_icon"></div>
+                                  </div>
+                                </a>
+                              </li>
+                              <li class="" style="" v-if="tabData.report_three_title">
+                                <a :href="tabData.report_three_file" download target="_blank">
+                                  <div class="f_icon">
+                                    <img src="./../../../assets/img/financial-report/Q3.png" alt="">
+                                  </div>
+                                  <div class="title">
+                                    <h6>{{tabData.report_three_title}}</h6>
+                                  </div>
+                                  <div class="download-icon" style="padding: 5px 32px;">
+                                    <div class="d_icon"></div>
+                                  </div>
+                                </a>
+                              </li>
+                              <li class="" style="" v-if="tabData.report_four_title">
+                                <a :href="tabData.report_four_file" download target="_blank">
+                                  <div class="f_icon">
+                                    <img src="./../../../assets/img/financial-report/Q4.png" alt="">
+                                  </div>
+                                  <div class="title">
+                                    <h6>{{tabData.report_four_title}}</h6>
+                                  </div>
+                                  <div class="download-icon" style="padding: 5px 32px;">
+                                    <div class="d_icon"></div>
+                                  </div>
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+
+                        </div>
+                        <!-- replace these with your preferred icons -->
+                        <strong v-if="isVisible" aria-hidden="true" class="ml-auto"><span class="icon"><span class="icon-caret-up" style="color: #fff; font-size: 18px;"></span></span></strong>
+                        <strong v-else aria-hidden="true" class="ml-auto"><span class="icon"><span class="icon-caret-down" style="color: #fff; font-size: 18px;"></span></span></strong>
+                      </a>
+                    </b-card>
+                  </b-collapse>
+
+                </b-card>
+              </div>
+          </div>
           </div>
         </div>
       </div>
